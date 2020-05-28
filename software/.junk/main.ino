797d326e283c83a656ea1f646e05dfb350403f1e
Kina Smith <kinasmith@pm.me>
2020-05-28 09:26:58 -0600

    starting code additions

--- software/.junk/main.ino
+++ software/.junk/main.ino
@@ -1,123 +1,107 @@
-/*
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
+/**
+ * Port: 1616
+ * IP Address: 192.168.0.140
+ * Input: 2 char Hex String converted to Binary
+ * Pin Outputs: 2,3,4,5, 6,7,8,9
+ * Also can do A0-A5 for an additional 6 outputs (make these Plated Thru Holes on PCB for additional Expansion possibilities)
  */
 
+#include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-IPAddress ip(192, 168, 1, 177);
+#define BUF_SIZE 8
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
+byte mac[] = { 0x89, 0xF0, 0x4F, 0xB0, 0x2C, 0x65 };
+IPAddress ip(192,168,1,55);
+EthernetServer server(1616);
+int eth_led = 2;
+bool eth_led_state = false;
 
-void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+String readRequest(EthernetClient*);
+void executeRequest(EthernetClient*, String*);
 
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Ethernet WebServer Example");
+/**
+ *  set led on when Ethernet Initilized. If not init, blink all @ rate
+ *  while waiting for TCP connection, pulse LED's to inidicate waiting...
+ *  if Ethernet cable not connected, blink ETH light @ rate
+ */
 
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
+void setup(){
+	for (int i = 2; i <= 9; i++) { //intialize output pins
+		pinMode(i, OUTPUT);
+	}
+	Serial.begin(9600);
+	// while(!Serial) {}
+	//blink all leds 5 times to inidicate Serial was successful.
+	for(int i = 0; i <= 5; i++) {
+		for (int i = 2; i <= 9; i++)
+			digitalWrite(i, HIGH);
+		delay(100);
+		for (int i = 2; i <= 9; i++)
+			digitalWrite(i, LOW);
+		delay(100);
+	}
 
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start the server
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+	Serial.println("begin");
+	/**
+	 * if Ethernet shield fails to initialized, blink the LED over the ethernet port forever
+	 */
+	Ethernet.init(10);
+	Ethernet.begin(mac, ip);
+	if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+		Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+		while (true) {
+			digitalWrite(eth_led, HIGH);
+			delay(100);
+			digitalWrite(eth_led, LOW);
+			delay(100);
+		}
+	}
+	/**
+	 * if Ethernet Cable is not plugged in, turn LED 2 on
+	 */
+	if (Ethernet.linkStatus() == LinkOFF){
+		digitalWrite(eth_led, HIGH);
+		Serial.println("Ethernet cable is not connected.");
+	} else digitalWrite(eth_led, LOW);
+	server.begin();
+	Serial.print("Server address:");
+	Serial.println(Ethernet.localIP());
 }
 
+void loop(){
+	EthernetClient client = server.available();
+	if(!client){
+		return;
+	}
+	Serial.println("Client connected");
+	String request = readRequest(&client); //gets Serial string
+	Serial.println(request); //prints that Serial String out
+	executeRequest(&client, &request);
 
-void loop() {
-  // listen for incoming clients
-  EthernetClient client = server.available();
-  if (client) {
-    Serial.println("new client");
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-    // close the connection:
-    client.stop();
-    Serial.println("client disconnected");
-  }
+	Serial.println("Client disonnected");
 }
 
+String readRequest(EthernetClient* client){
+	String request = "";
+	while (client->connected()){
+		while (client->available()){
+			char c = client->read();
+			// Serial.write(c); //prints recieved characters
+			if ('\n' == c){
+				return request;
+			}
+			request += c;
+		}
+	}
+	return request;
+}
+void executeRequest(EthernetClient* client, String* request){
+	for(int i = 0; i < BUF_SIZE; i++) {
+		int pinState = request->charAt(i);
+		int pinNum = (BUF_SIZE-i)+1;
+		if(pinState == '1') digitalWrite(pinNum, HIGH);
+		else if(pinState == '0') digitalWrite(pinNum, LOW);
+	}
+}
--- software/src/test.cpp
+++ software/src/test.cpp
@@ -13,49 +13,74 @@
 #define BUF_SIZE 8
 
 byte mac[] = { 0x89, 0xF0, 0x4F, 0xB0, 0x2C, 0x65 };
-IPAddress ip(192,168,1,55);
+IPAddress ip(192,168,1,58);
 EthernetServer server(1616);
-
-
+int status_led_pin = 13;
+bool status_led_state = false;
 
 String readRequest(EthernetClient*);
 void executeRequest(EthernetClient*, String*);
 
+/**
+ *  set led on when Ethernet Initilized. If not init, blink all @ rate
+ *  while waiting for TCP connection, pulse LED's to inidicate waiting...
+ *  if Ethernet cable not connected, blink ETH light @ rate
+ * 
+ * Send Acknowledgements on successful write outputs
+ * RCV 'r' and send state of all 8 outputs
+ * 	use digitalRead() w/o setting pinMode to OUTPUT to read pinState Register
+ */
+
 void setup(){
-	Ethernet.init(10);
+	for (int i = 2; i <= 9; i++) { //intialize output pins
+		pinMode(i, OUTPUT);
+	}
 	Serial.begin(9600);
-	// while(!Serial) {}d
+	while(!Serial) {}
+	//blink all leds 5 times to inidicate Serial was successful.
+	for(int i = 0; i <= 5; i++) {
+		digitalWrite(status_led_pin, HIGH);
+		delay(100);
+		digitalWrite(status_led_pin, LOW);
+		delay(100);
+	}
+
 	Serial.println("begin");
+	/**
+	 * if Ethernet shield fails to initialized, blink the LED over the ethernet port forever
+	 */
+	Ethernet.init(10);
 	Ethernet.begin(mac, ip);
 	if (Ethernet.hardwareStatus() == EthernetNoHardware) {
 		Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
 		while (true) {
-		delay(1); 
+			digitalWrite(status_led_pin, HIGH);
+			delay(100);
+			digitalWrite(status_led_pin, LOW);
+			delay(100);
 		}
 	}
+	/**
+	 * if Ethernet Cable is not plugged in, turn LED 2 on
+	 */
 	if (Ethernet.linkStatus() == LinkOFF){
+		digitalWrite(status_led_pin, HIGH);
 		Serial.println("Ethernet cable is not connected.");
-	}
+	} else digitalWrite(status_led_pin, LOW);
 	server.begin();
 	Serial.print("Server address:");
 	Serial.println(Ethernet.localIP());
-	for(int i = 2; i <= 9; i++) { //intialize output pins
-		pinMode(i, OUTPUT);
-		// Serial.println(i);
-	}
 }
 
 void loop(){
-
 	EthernetClient client = server.available();
-	if (!client){
+	if(!client){
 		return;
 	}
 	Serial.println("Client connected");
 	String request = readRequest(&client); //gets Serial string
 	Serial.println(request); //prints that Serial String out
 	executeRequest(&client, &request);
-
 	Serial.println("Client disonnected");
 }
 
@@ -64,7 +89,7 @@ String readRequest(EthernetClient* client){
 	while (client->connected()){
 		while (client->available()){
 			char c = client->read();
-			// Serial.write(c); //prints recieved characters
+			Serial.write(c); //prints recieved characters
 			if ('\n' == c){
 				return request;
 			}
